524c65d387c92fd9e787c2cc0a9b85fb49705c1c
itsmaleficent <159501426+itsmaleficent@users.noreply.github.com>
2024-02-19 00:57:05 +0200

    elegant index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to Slappy: http://localhost:${port}`);
+  console.log(`🔗 Powered By Slappy`);
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Elegant Community","Elegant Services"];
 
 
 let currentIndex = 0;
@@ -63,7 +63,7 @@ GIT : https://github.com/RTX-GAMINGG/Bot-ghost-status-remover-by-RTX
   DISCORD SERVER : https://discord.gg/FUEHs7RCqz
   YOUTUBE : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
  * **********************************************
- *   Code by RTX GAMING
+ *   Code by Elegant Community
  * **********************************************
  */
 
